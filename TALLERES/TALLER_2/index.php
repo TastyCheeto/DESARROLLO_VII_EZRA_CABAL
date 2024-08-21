34697d0e75ba69016942f45a12d36d03d95b7011
Ezra Cabal <ezra.cabal@utp.ac.pa>
2024-08-20 19:45:03 -0500

    Definidas variables de diferentes tipos

--- TALLERES/TALLER_2/index.php
+++ TALLERES/TALLER_2/index.php
@@ -1,4 +1,13 @@
+
 <?php
-// Tu código PHP irá aquí y otras cosas más
+$nombre = "Ezra";
+$edad = 23;
+$altura = 1.82;
+$esEstudiante = true;
+
+echo "Nombre: $nombre<br>";
+echo "Edad: $edad<br>";
+echo "Altura: $altura<br>";
+echo "¿Es estudiante? " . ($esEstudiante ? "Sí" : "No");
 ?>
-                        
+                    
